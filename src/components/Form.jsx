0dd6ca593749f970bbba4336a02b0bd934b98e97
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-18 20:40:21 -0300

    eventos e hocks

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
+
 export function Form() {
   function cadastrarUsuario(e) {
-      e.preventDefault()
+    e.preventDefault();
+    console.log(name)
+    console.log(password)
     console.log("me cadastrou aqui pai");
   }
+
+  const [name, setName] = useState()
+  const [password, setPassword] = useState()
+
   return (
     <>
       <h1>Meu cadastro</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="digite seu nome" />
+          <label htmlFor="name">
+            Nome:
+            <input 
+              type="text" 
+              id="name" 
+              placeholder="digite seu nome"
+              onChange={(e) => setName(e.target.value)}/>
+          </label>
         </div>
+
         <div>
-          <input type="submit" value="cadastrar" />
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              id="password"
+              placeholder="digite sua senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="submit">
+            <input type="submit" id="submit" value="cadastrar" />
+          </label>
         </div>
       </form>
     </>
